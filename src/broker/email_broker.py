be82b8e059ae5622932bad93663bc6da6f3b5e15
Víctor Colombo <victorcora98@gmail.com>
2020-07-15 17:30:55 -0300

    Finish mailer 1.0

---
+++ src/broker/email_broker.py
@@ -0,0 +1,55 @@
+import logging
+import pika
+import json
+import os
+
+from mailer.mailer import Mailer, MailerException
+from mailer.email_message import EmailMessage
+
+class EmailBroker:
+    RABBITMQ_USER = os.environ["RABBITMQ_USER"]
+    RABBITMQ_PASS = os.environ["RABBITMQ_PASS"]
+    QUEUE = "emails"
+
+    logger = logging.getLogger(__name__)
+
+    def __init__(self):
+        credentials = pika.PlainCredentials(
+            self.RABBITMQ_USER, self.RABBITMQ_PASS)
+        self._params = pika.ConnectionParameters(
+            "rabbitmq", 5672, '/', credentials)
+        self._conn = None
+        self._channel = None
+
+    def connect(self):
+        if not self._conn or self._conn.is_closed:
+            self._conn = pika.BlockingConnection(self._params)
+            self._channel = self._conn.channel()
+            self._channel.queue_declare(queue=self.QUEUE, durable=True)
+
+    def close(self):
+        if self._conn and self._conn.is_open:
+            self.logger.info('closing queue connection')
+            self._conn.close()
+
+    def consume(self):
+        self._channel.basic_consume(
+            queue=self.QUEUE, on_message_callback=self._callback)
+        self._channel.start_consuming()
+
+    def _callback(self, ch, method, properties, body):
+        try:
+            data = json.loads(body)
+
+            email_message = EmailMessage(
+                data["email"], data["subject"], data["html"])
+            Mailer().send_email(email_message)
+
+            ch.basic_ack(delivery_tag = method.delivery_tag)
+        except MailerException as e:
+            self.logger.exception(e)
+            ch.basic_nack(delivery_tag = method.delivery_tag)
+        except Exception as e:
+            self.logger.exception(e)
+            ch.basic_reject(
+                delivery_tag=method.delivery_tag, requeue=False)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 import logging
 
-from mailer.mailer import Mailer
-from mailer.email_message import EmailMessage
+from broker.email_broker import EmailBroker
 
-logging.basicConfig(level=logging.INFO)
+def main():
+    logging.basicConfig(level=logging.INFO)
 
-email_message = EmailMessage("victorcora98@gmail.com", "test", "test")
-Mailer().send_email(email_message)
+    broker = EmailBroker()
+    broker.connect()
+    broker.consume()
+    broker.close()
+
+if __name__ == "__main__":
+    main()
